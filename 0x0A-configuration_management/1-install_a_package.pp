8d3ab0b93e5fe75c4945a6d9aec571ad9c61bb5a
ouidane <ouidanezakaria@gmail.com>
2025-01-11 05:21:33 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # install flask from pip3
 
-package { 'flask' :
+package {'flask':
 ensure   => '2.1.0',
-provider => 'pip3',
+name     => 'flask',
+provider => pip3,
+}
+
+package {'werkzeug':
+ensure   => '2.1.1',
+name     => 'Werkzeug',
+provider => pip3,
 }
