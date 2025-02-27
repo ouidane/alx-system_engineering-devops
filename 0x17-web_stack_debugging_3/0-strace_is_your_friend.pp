cf5b9c23c9f3c0dc614c41d1ddd4d4f603d0a65d
ouidane <ouidanezakaria@gmail.com>
2025-02-27 01:33:09 +0000

    Fixing 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# automated puppet fix to find out why Apache is returning a 500 error
+# fixing apache 500 error
 
 exec { 'Fixing 500 error':
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
