3f25cc1481384d484cb5fa1f49494b5c56274c57
ouidane <ouidanezakaria@gmail.com>
2025-02-27 02:55:48 +0000

    Fixing 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Puppet manifest to fix Apache 500 error
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/bin:/bin:/usr/bin',
+  onlyif  => "grep -q '.phpp' /var/www/html/wp-settings.php",
 }
